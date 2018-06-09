0fb38d3d51ed98f5ee1d47d11253dcb0874f1ed8
Jessica La Torre <jess.latorre@gmail.com>
2018-06-08 22:45:21 -0500

    updated CSS

--- server.js
+++ server.js
@@ -13,12 +13,15 @@ var app = express();
 // Serve static content for the app from the 'public' directory
 app.use(express.static(process.cwd() + '/public'));
 
+//set up express to parse html requrest. but review documentation
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Override with POST having ?_method=DELETE
 app.use(methodOverride('_method'));
 
 // Set Handlebars as the view engine; this is an express-handlebars npm module
+//express-handlebars is the node module, so we can handle handlebars in express.
+//we use this in 
 var exphbs = require('express-handlebars');
 
 //set up engine to run handlebars. In documentation: Note: the route is in the routes file: not in this one. Example of documentation
@@ -36,6 +39,8 @@ var exphbs = require('express-handlebars');
         // });
         
         // app.listen(3000);
+        //set up handlebars as view enginer - documentation
+        //also telling it to layout to look at main.handlebars
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
